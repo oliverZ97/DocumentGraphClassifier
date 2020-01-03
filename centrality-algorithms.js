16d0b43f3d76c347dce7dcf95db2535d12ac8871
OliverZ97 <oliver@mordoor.de>
2020-01-03 09:29:55 +0100

    write documentation for centrality-algorithms.js

--- centrality-algorithms.js
+++ centrality-algorithms.js
@@ -3,6 +3,12 @@ const g = require("ngraph.graph")();
 const dbManager = require("./dbManager")
 var dbm = new dbManager();
 /******************************************************************************************************/
+/*
+*description: this function is the entrypoint of the script. It starts a call to the database by call 
+*the dbManager to receive all nodes in the actual graph. The result is used in the next step.
+*@param: 
+*@return: 
+*/
 module.exports = getAllNodesFromDB = function() {
     let nodes = dbm.getAllNodes()
     nodes.then(function (result) {
@@ -13,6 +19,15 @@ module.exports = getAllNodesFromDB = function() {
     })
 }
 /******************************************************************************************************/
+//https://www.npmjs.com/package/ngraph.graph
+//https://www.npmjs.com/package/ngraph.centrality
+/*
+*description: this function handles the calculation of the graphs metrics and the insertion of the 
+*results to the database. Using the addLink Method, a graph is created out of the objects given by nodeSets. 
+*Using centrality different metrics are calculated on graph object g.
+*@param: nodeSets {array} - containing multiple objects, each representing two nodes with a connection.
+*@return: 
+*/
 function computeCentralityAlgorithms(nodeSets) {
     nodeSets.forEach(element => {
         let subject = removeUri(element.s.value);
@@ -30,6 +45,11 @@ function computeCentralityAlgorithms(nodeSets) {
     dbm.insertCentrality(betweenessCentrality, "betweenessCentrality");
 }
 /******************************************************************************************************/
+/*
+*description: replaces the whole IRI except the fragment and replaces it with a placeholder.
+*@param: string {string} - a string representing an IRI.
+*@return: string {string} - the transformed IRI.
+*/
 function removeUri(string) {
     let newString = "";
     if (string.includes("http://example.org/dgc#")) {
